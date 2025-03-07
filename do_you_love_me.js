95cb10d5b432518c8cfbdd73707e2167f55efaaf
ChienBug <chienne07122003@gmail.com>
2025-03-07 12:27:45 +0700

    Update do_you_love_me.js

--- do_you_love_me.js
+++ do_you_love_me.js
@@ -8,7 +8,7 @@ const noBtn = document.querySelector(".js-no-btn");
 // /change the postion of no button
 noBtn.addEventListener("mouseover", () => {
   const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
-  const newY = Math.floor(Math.random() * questionContainer.offsetWidth);z
+  const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
 
   noBtn.style.left = `${newX}px`;
   noBtn.style.top = `${newY}px`;
