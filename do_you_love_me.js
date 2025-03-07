23da42d143f249c7c34bbd3610bdf06e478d9b43
ChienBug <chienne07122003@gmail.com>
2025-03-07 12:48:07 +0700

    Update do_you_love_me.js

--- do_you_love_me.js
+++ do_you_love_me.js
@@ -5,24 +5,25 @@ const heartLoader = document.querySelector(".cssload-main");
 const yesBtn = document.querySelector(".js-yes-btn");
 const noBtn = document.querySelector(".js-no-btn");
 
-// /change the postion of no button
+// Thay đổi vị trí nút "No"
 noBtn.addEventListener("mouseover", () => {
   const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
-  const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
+  const newY = Math.floor(Math.random() * questionContainer.offsetHeight);
 
+  noBtn.style.position = "absolute"; // Đảm bảo nút có thể di chuyển
   noBtn.style.left = `${newX}px`;
   noBtn.style.top = `${newY}px`;
 });
 
-// yes button functionality
-
+// Xử lý khi bấm "Yes"
 yesBtn.addEventListener("click", () => {
   questionContainer.style.display = "none";
   heartLoader.style.display = "inherit";
 
-  const timeoutId = setTimeout(() => {
+  setTimeout(() => {
     heartLoader.style.display = "none";
-    resultContainer.style.display = "block";
-    gifResult?.play();
+    resultContainer.classList.add("show"); // Thêm class hiển thị
+    console.log(gifResult); // Kiểm tra xem có lấy được video không
+    gifResult?.play(); // Chạy video nếu tồn tại
   }, 3000);
 });
